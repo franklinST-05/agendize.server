dd31250b14cdf02d995d3e334056caae9aa3595d
franklinST-05 <franklinteixeira205@gmail.com>
2023-04-09 20:09:55 -0300

    feat:

---
+++ src/settings/server/index.ts
@@ -0,0 +1,12 @@
+import express from "express";
+import dotenv from "dotenv";
+import { routes } from "../routes";
+
+const server = express();
+dotenv.config();
+
+server.use(express.json());
+server.use(express.urlencoded({ extended: true }));
+server.use(routes);
+
+export { server };
